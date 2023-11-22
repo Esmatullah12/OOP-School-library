2c41b99071aa68d625fd013be46c218b8442f7cf
Esmatullah <esmatqadeemi12@gmail.com>
2023-11-22 11:27:47 +0100

    Completed the required changes

--- person.rb
+++ person.rb
@@ -2,15 +2,15 @@ class Person
   attr_reader :id
   attr_accessor :name, :age
 
-  def initialize(id, age, name: 'Unknwon', parent_permission: true)
-    @id = id
+  def initialize(_id, age, name = 'Unknwon', parent_permission: true)
+    @id = Random.rand(1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
   end
 
   def can_use_services
-    true
+    of_age? || @parent_permission
   end
 
   private
