7b7da78dc1e6ccdea58ba24ef54c7072eca18b02
Esmatullah <esmatqadeemi12@gmail.com>
2023-11-22 11:52:30 +0100

    Fix the linter error

--- teacher.rb
+++ teacher.rb
@@ -1,7 +1,7 @@
 require_relative 'person'
 
 class Teacher < Person
-  def initialize(id, age, name = "Unknows", parent_permission, specialization)
+  def initialize(id, age, parent_permission, specialization, name = 'Unknows')
     super(id, age, name, parent_permission)
     @specialization = specialization
   end
