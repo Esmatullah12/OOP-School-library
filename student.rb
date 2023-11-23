530176a4d79261d49cd69c8155a38e1bdb1530b2
Esmatullah <esmatqadeemi12@gmail.com>
2023-11-23 12:21:22 +0100

    Created relations between class

--- student.rb
+++ student.rb
@@ -1,9 +1,16 @@
 require_relative 'person'
 
 class Student < Person
-  def initialize(id, age, classroom, name = 'Unknown', parent_permission: true)
+  attr_accessor :classroom
+
+  def initialize(id, age, _classroom, name = 'Unknown', parent_permission: true)
     super(id, age, name, parent_permission)
+    @classroom = nil
+  end
+
+  def assign_to_classroom(classroom)
     @classroom = classroom
+    classroom.add_student(self)
   end
 
   def play_hooky
